2f15b90469cb026b84aac494d2b8d425b4a176d4
Ned Palacios <npdoesmc@gmail.com>
2020-11-10 11:11:59 +0800

    Refactoring (#1)
    * Remove redundant os import
    
    ===
    
    Sa code nmo, duha ka beses ka nag-import ug os module: ang isa tung whole module and the other one is katung scoped one which tung `mkdir`. Since duha lang ka functions imong ggamit sa namentioned na module (mkdir and listdir), iremove nato tung first one and iapil nalang nato sya ddto sa existing na scoped import pra dli sya redundant.
    
    * Extract most common code into functions
    
    ===
    
    Nakit-an nako na naay parts sa code na naga-usab2x ra so gi-extract nako to sila into separate functions. Ang first one is tung giformat ang string and the other one is tung naga-extract sa data into a dictionary. Ang technique lang nako ana is gina Cmd/Ctrl+F lang nako if naa bay similar instance sa codebase.
    
    * Create a single const/variable for mostly used folders (part 1)
    
    ===
    
    Nakapansin ko na perminte ka naga-gamit ug `company_data` na folder name. So ang ghimo nako is at the very top sa program code, nagdeclare kog constant (variable w/ fixed value) named `COMPANY_DATA_FOLDER` and ddto nako gistore ang pangalan sa folder sa company.
    
    The reason with this is that para malessen ang chances na mamistyped nmo tung location sa folder and at the same time we have a so-called "Single source of truth": isa lang atong gnabasehan and ddto lang gkan atung gireference na variable.
    
    * Create a single const/variable for mostly used folders (part 2)
    
    ===
    
    Same explanation pero gi-apply nato sa employees folder. Ang kalahian diri is that if naka-pansin ka on every line on the red side is nagagamit ug same function pang-format sailahang filename so ang gihimo nako is gbutang nako directly sa `employee_file_location` na function tung pag-invoke sa `fix_text_format` pra malessen tung redundant na code.
    
    * Extract more functions
    
    ===
    
    Nakabantay ko na naay ~11 instances nagprint ka ug lines so giextract nako sya into function pra dli sd libog.
    
    * Fix invalid code

--- main2.py
+++ main2.py
@@ -1,86 +1,98 @@
-import os
 import shutil
 import sys
-from os import mkdir
+from os import mkdir, listdir
 from os.path import exists as fileExists
 
-#   FUNCTIONS
-def salary_read(user):
-    username = user
-    filename = "./employees_data/{}.txt".format(username.lower().replace(" ", "_").replace(".", "_"))
-    file = open(filename, "r")
-    contents = file.readlines()
-    fields = {}
+COMPANY_DATA_FOLDER = "company_data/"
+EMPLOYEES_DATA_FOLDER = "employees_data/"
+running = True
 
+#   FUNCTIONS
+def company_file_location(filename):
+    return "./{}{}.txt".format(COMPANY_DATA_FOLDER, filename)
+
+def employee_file_location(filename):
+    return "./{}{}.txt".format(EMPLOYEES_DATA_FOLDER, fix_text_format(filename))
+
+def fix_text_format(user):
+    return user.lower().replace(" ", "_").replace(".", "_")
+
+def extract_data(contents):
+    fields = {}
     for line in contents:
         values = line.split(": ")
         fields[values[0]] = values[1]
+    return fields
 
-        est_salary = fields["EST. SALARY"]
+def print_lines():
+    print("-" * 50)
+
+def print_dir_contents(folder):
+    dir_list = listdir(folder)
+    print(dir_list)
+    print_lines()
+
+def print_message(msg):
+    print(msg)
+    print_lines()
+
+def salary_read(user):
+    filename = employee_file_location(user)
+    file = open(filename, "r")
+    contents = file.readlines()
+    fields = extract_data(contents)    
+
+    est_salary = fields["EST. SALARY"]
+    salary_bracketing(est_salary)
+
+    if len(contents) > 0:
+        print("".join(contents))
+        print("Estimated Salary: {}".format(fields["EST. SALARY"]))
+
         salary_bracketing(est_salary)
-
-        if len(contents) > 0:
-            print("".join(contents))
-            print("Estimated Salary: {}".format(fields["EST. SALARY"]))
-
-        salary_bracketing(est_salary)
-
-
 
     file.close()
 
 def info_update(user):
-    username = user
-    filename = "./employees_data/{}.txt".format(username.lower().replace(" ", "_").replace(".", "_"))
+    filename = employee_file_location(user)
     file = open(filename, "r")
     contents = file.readlines()
-    fields = {}
-
-    for line in contents:
-        values = line.split(": ")
-        fields[values[0]] = values[1]
+    fields = extract_data(contents)
 
     if len(contents) > 0:
         print("".join(contents))
         print("Monthly Salary: {}".format(fields["MONTHLY SALARY"]))
+
     file.close()
 
 def salary_bracketing(emp_salary):
     global employee_position
     est_salary = emp_salary
-
-    fields = {}
-    for line in contents:
-        values = line.split(": ")
-        fields[values[0]] = values[1]
-
-        employee_position = fields["Company Position"]
+    fields = extract_data(contents)
+    employee_position = fields["Company Position"]
 
     if est_salary <=20000:
-        filename = "./company_data/{}.txt".format(str(employee_position).replace(" ", "_").replace(".", "_"))
-        for line in contents:
-            values = line.split(": ")
-            fields[values[0]] = values[1]
-            employee_position = fields["Deductions"]
+        filename = company_file_location(employee_position.replace(" ", "_").replace(".", "_"))
+        company_fields = extract_data(contents)
+        employee_position = fields["Deductions"]
 
-running = True
 while running:
-
     #   PROGRAM EXISTENCE CHECK:
-    filename = "./company_data/.txt"
+    filename = "./{}/.txt".format(COMPANY_DATA_FOLDER)
     if fileExists(filename):
         continue
-    if not fileExists("company_data/"):
+
+    if not fileExists(COMPANY_DATA_FOLDER):
         prompt_1 = input("Company data not found! Enroll your company? (Y/N): ").upper()
         if prompt_1 == "Y":
-            mkdir("company_data/")
+            mkdir(COMPANY_DATA_FOLDER)
             company_name = input("Enter Company Name: ")
             prompt_2 = int(input("Enter number of available positions: "))
             positionNum = prompt_2
             for i in range(positionNum):
                 company_positions = input("Enter position: ")
                 est_salary = int(input("Monthly salary: "))
-                filename = "./company_data/{}.txt".format(company_positions.replace(" ", "_").replace(".", "_"))
+                filename = company_file_location(company_positions.replace(" ", "_").replace(".", "_"))
                 file = open(filename, "w")
                 file.write("Company Position: {}\n".format(company_positions))
                 file.write("Salary: {}\n".format(est_salary))
@@ -94,8 +106,7 @@ while running:
             sys.exit()
 
     #   MAIN MENU
-    print("Welcome to Bonapay Payroll Management System")
-    print("-"*50)
+    print_message("Welcome to Bonapay Payroll Management System")
     print("Select an action: \n"+"(1) Review company data \n"+"(2) Enroll employee \n"+
           "(3) Read employee data \n"+"(4) Delete employee data\n"+"(5) Reset program\n"+
           "(6) Exit")
@@ -103,17 +114,14 @@ while running:
     print("")
 
     if cmd == 1:
-        print("-" * 50)
+        print_lines()
         print("Available Company data: ")
-        dir_list = os.listdir(path='company_data/')
-        print(dir_list)
-        print("-" * 50)
+        print_dir_contents(COMPANY_DATA_FOLDER)
 
-        post_input = input("To read data, enter position name: ")
-        filename = "./company_data/{}.txt".format(post_input.lower().replace(" ", "_").replace(".", "_"))
+        post_input = fix_text_format(input("To read data, enter position name: "))
+        filename = company_file_location(post_input)
         if not fileExists(filename):
-            print("Data not found!")
-            print("-" * 50)
+            print_message("Data not found!")
             continue
 
         file = open(filename, "r")
@@ -124,24 +132,19 @@ while running:
 
     #   OPT #2 : WRITING DATA
     elif cmd == 2:
-        if not fileExists("employees_data/"):
-            mkdir("employees_data/")
+        if not fileExists(EMPLOYEES_DATA_FOLDER):
+            mkdir(EMPLOYEES_DATA_FOLDER)
         employee_fName = input("First Name: ")
         employee_lName = input("Last Name: ")
         employee_idNum = input("Employee I.D.: ")
-        filename = "./employees_data/{}.txt".format(employee_idNum.replace(" ", "_").replace(".", "_"))
+        filename = employee_file_location(employee_idNum)
 
         #   FILE EXISTENCE CHECK:
         if fileExists(filename):
-            print("Employee already exists!")
-            print("-"*50)
+            print_message("Employee already exists!")
             continue
-
-        dir_list = os.listdir(path='company_data/')
-        print(dir_list)
-        print("-" * 50)
+        
         employee_position = (input("Enter Position: "))
-
         employee_absences = int(input("Number of Absences: "))
         employee_overtimeHrs = int(input("Number of Overtime Hours: "))
         employee_late = int(input("Number of Times Late (in mins.): "))
@@ -155,35 +158,27 @@ while running:
         file.write("Num. of Overtime Hours: {}\n".format(employee_overtimeHrs))
         file.write("Num. of Times Late (in mins.): {}\n".format(employee_late))
 
-        filename = "./company_data/{}.txt".format(employee_position.lower().replace(" ", "_").replace(".", "_"))
-
+        filename = company_file_location(fix_text_format(employee_position))
         contents = file.readlines()
-        fields = {}
-        for line in contents:
-            values = line.split(": ")
-            fields[values[0]] = values[1]
-
-        employee_salary = fields["Salary: "]
+        fields = extract_data(contents)
+        employee_salary = fields["Salary"]
 
         file.write("EST. SALARY: {}\n".format(employee_salary))
         file.close()
 
-        print("-"*50)
-        print("Employee data saved successfully!")
-        print("-"*50)
+        print_lines()
+        print_message("Employee data saved successfully!")
 
     #   READ DATA:
     elif cmd == 3:
-        print("-"*50)
+        print_lines()
         print("Available employee data: ")
-        print(os.listdir(path='employees_data/'))
-        print("-"*50)
+        print_dir_contents(EMPLOYEES_DATA_FOLDER)
 
         id_input = input("To read data, enter employee's I.D. Number: ")
-        filename = "./employees_data/{}.txt".format(id_input.lower().replace(" ", "_").replace(".", "_"))
+        filename = employee_file_location(id_input)
         if not fileExists(filename):
-            print("Employee data not found!")
-            print("-"*50)
+            print_message("Employee data not found!")
             continue
 
         # Calls function 'salary_read' and uses I.D. as parameter
@@ -191,17 +186,14 @@ while running:
 
     #   DELETE EMPLOYEE DATA
     elif cmd == 4:
-        print("-" * 50)
+        print_lines()
         print("Available Company data: ")
-        dir_list = os.listdir(path='company_data/')
-        print(dir_list)
-        print("-" * 50)
+        print_dir_contents(COMPANY_DATA_FOLDER)
 
-        post_input = input("To read data, enter position name: ")
-        filename = "./company_data/{}.txt".format(post_input.lower().replace(" ", "_").replace(".", "_"))
+        post_input = fix_text_format(input("To read data, enter position name: "))
+        filename = company_file_location(post_input)
         if not fileExists(filename):
-            print("Data not found!")
-            print("-" * 50)
+            print_message("Data not found!")
             continue
 
         file = open(filename, "r")
@@ -217,11 +209,9 @@ while running:
         if cmd_1 == "Y":
             if fileExists("employees_data"):
                 shutil.rmtree("employees_data")
-            print("Employee data deleted succesfully!")
-            print("-" * 50)
+                print_message("Employee data deleted succesfully!")
             shutil.rmtree("company_data")
-            print("Company data deleted succesfully!")
-            print("-" * 50)
+            print_message("Company data deleted succesfully!")
 
     #   EXIT
     elif cmd == 6:
